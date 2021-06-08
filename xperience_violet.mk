2cb483a37c0084b508cb4ec576f4511d3f4f588a
Kesavan <kesavang4020@gmail.com>
2021-06-08 15:31:04 +0530

    Violet:Updated FP to June RQ3A.210605.005

--- xperience_violet.mk
+++ xperience_violet.mk
@@ -24,8 +24,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ2A.210505.003 7255357 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005 7349499 release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)" \
