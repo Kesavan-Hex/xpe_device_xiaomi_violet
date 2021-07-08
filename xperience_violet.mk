6c41989f12100e9db57ca8988089a434e38203b0
KesavanG <73026586+Kesavan-Hex@users.noreply.github.com>
2021-07-08 09:53:54 +0530

    Updated FingerPrint to July Redfin RQ3A.210705.001

--- xperience_violet.mk
+++ xperience_violet.mk
@@ -24,8 +24,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005 7349499 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210705.001 7380771 release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)" \
